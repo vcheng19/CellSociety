5df8e0928eb1e84028753a839a97603f95008352
Cody <codyli332@gmail.com>
2016-02-02 22:40:37 -0500

    Wrote methods to determine the location of the cell, will definitely go back to improve efficiency

--- src/cellsociety_team24/RuleEnforcer.java
+++ src/cellsociety_team24/RuleEnforcer.java
@@ -4,11 +4,56 @@ import java.util.*;
 
 
 public abstract class RuleEnforcer {
-
+	int gridsize; //FILLER
 	public RuleEnforcer() {
 		// TODO Auto-generated constructor stub
 		
-		
 	}
-
+	
+	public void checkNeighbor(Cell cell){
+		int[] cellCoord = cell.getCoordinates() //I think we should add this method in 
+		
+		if(isCornerCell(cellCoord[0], cellCoord[1])){
+			
+		}
+		
+		else if(isMiddleCell(cellCoord[0], cellCoord[1])){
+		}
+		else{ //Side cell 
+			
+		}
+			
+	}
+	
+	public boolean isCornerCell(int r, int c){
+		if(r == 0 || r == gridsize - 1){
+			if(c == 0 || c == gridsize - 1){
+				return true;
+			}
+			else{
+				return false; 
+			}
+		}
+		else{
+			return false; 
+		}
+	}
+	
+	public boolean isMiddleCell(int r, int c){
+		if(r != 0 || r != gridsize - 1){
+			if(c != 0 || c != gridsize - 1){
+				return true; 
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			return false; 
+		}
+	}
+	
+//	public boolean isSideCell(int r, int c){
+//		return true; 
+//	}
 }
