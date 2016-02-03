c3e406b588a8df880b8f7f2e038bc3da53c6ac7c
Cody <codyli332@gmail.com>
2016-02-03 14:09:08 -0500

    Wrote the checkNeighbors method for GOLRuleEnforcer.

--- src/cellsociety_team24/GOLRuleEnforcer.java
+++ src/cellsociety_team24/GOLRuleEnforcer.java
@@ -1,11 +1,35 @@
 package cellsociety_team24;
 
+import java.util.*;
+
 public class GOLRuleEnforcer extends RuleEnforcer {
-	private Cell[][] myGrid; 
-	public GOLRuleEnforcer(Cell[][]grid){
-		super(Cell[][] grid);
+	private GameOfLifeCell[][] myGrid; 
+	
+	public GOLRuleEnforcer(GameOfLifeCell[][]grid){
+		super(grid);
 		myGrid = grid; 
-		
+	}
+	public GameOfLifeCell [][] iterateGrid(){
+		for(int r = 0; r < myGrid.length; r++){
+			for(int c = 0; c < myGrid.length; c++){
+				
+			}
+		}
+		return myGrid; 
+	}
+	
+	public int checkNeighbor(int r, int c){
+		int numDeadCells = 0;
+		for(int rChange = -1;rChange < 2;rChange++){
+			if(r + rChange >= 0 && r + rChange < myGrid.length){//If r is within the boundaries of the grid
+				for(int cChange = -1;cChange < 2;cChange++){
+					if(c + cChange >= 0 && r + cChange < myGrid.length){
+						if(myGrid[r+rChange][c+cChange].isDead()) numDeadCells++;
+					}
+				}
+			}	
+		}
+		return numDeadCells; 
 	}
 	
 
--- src/cellsociety_team24/RuleEnforcer.java
+++ src/cellsociety_team24/RuleEnforcer.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 
 public abstract class RuleEnforcer {
-	Cell[][] myGrid; 
-	Cell cell; 
+	private Cell[][] myGrid; 
+	private Cell cell; 
 	public RuleEnforcer(Cell[][] grid) {
 		myGrid = grid; 
 		// TODO Auto-generated constructor stub
@@ -32,36 +32,4 @@ public abstract class RuleEnforcer {
 	public void checkNeighbors(int xPos, int yPos){//Will need to update this 
 		return; 
 	}
-	
-//	public boolean isCornerCell(int r, int c){
-//		if(r == 0 || r == gridsize - 1){
-//			if(c == 0 || c == gridsize - 1){
-//				return true;
-//			}
-//			else{
-//				return false; 
-//			}
-//		}
-//		else{
-//			return false; 
-//		}
-//	}
-//	
-//	public boolean isMiddleCell(int r, int c){
-//		if(r != 0 || r != gridsize - 1){
-//			if(c != 0 || c != gridsize - 1){
-//				return true; 
-//			}
-//			else{
-//				return false;
-//			}
-//		}
-//		else{
-//			return false; 
-//		}
-//	}
-//	
-//	public boolean isSideCell(int r, int c){
-//		return true; 
-//	}
 }
