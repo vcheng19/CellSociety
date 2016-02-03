332fa8b4f919455d75e32f740e027eae119dc5e6
vcheng19 <vc54@duke.edu>
2016-02-02 23:27:52 -0500

    Updated Cell

--- src/cellsociety_team24/Cell.java
+++ src/cellsociety_team24/Cell.java
@@ -1,13 +1,23 @@
 package cellsociety_team24;
 
+import javafx.scene.Group;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public abstract class Cell {
 	Color color;
+	private int myX;
+	private int myY;
+	Rectangle myRect;
 	
-	public Cell() {
+	
+	public Cell(Group root, double size, int x, int y) {
 		// TODO Auto-generated constructor stub
-		color = Color.BLACK;
+		myX = x;
+		myY = y;
+		
+		Rectangle myRect = new Rectangle(x*size, y*size, size, size);
+		myRect.setFill(Color.BLACK);
 	}
 	
 	/**public Cell(Color c){
@@ -16,11 +26,18 @@ public abstract class Cell {
 	
 	public void setColor(Color c){
 		color = c;
-	}	*/
+	}	
 	
 	public Color getColor(){
 		return color;
+	} */
+	
+	public int getX(){
+		return myX;
 	}
 	
+	public int getY(){
+		return myY;
+	}
 
 }
