d321807a03136ab4f556e4a8b35c251f5216f990
Cody <codyli332@gmail.com>
2016-02-03 15:14:47 -0500

    Completed all methods under GOLRuleEnforcer class, still need to test for functionality

--- src/cellsociety_team24/GOLRuleEnforcer.java
+++ src/cellsociety_team24/GOLRuleEnforcer.java
@@ -12,24 +12,35 @@ public class GOLRuleEnforcer extends RuleEnforcer {
 	public GameOfLifeCell [][] iterateGrid(){
 		for(int r = 0; r < myGrid.length; r++){
 			for(int c = 0; c < myGrid.length; c++){
-				
+				int x = myGrid[r][c].getX(); 
+				int y = myGrid[r][c].getY(); 
+				//If alive and have 2 or 3 living neighbors
+				if(!myGrid[r][c].isDead() && (checkNeighbor(x, y) == 2 || checkNeighbor(x, y) == 3)){
+					myGrid[r][c].makeAlive(); 
+				}
+				else{
+					myGrid[r][c].killCell(); 
+				}
+				if(myGrid[r][c].isDead() && checkNeighbor(x, y) == 3){
+					myGrid[r][c].makeAlive(); 
+				}
 			}
 		}
 		return myGrid; 
 	}
 	
 	public int checkNeighbor(int r, int c){
-		int numDeadCells = 0;
+		int numAliveCells = 0;
 		for(int rChange = -1;rChange < 2;rChange++){
 			if(r + rChange >= 0 && r + rChange < myGrid.length){//If r is within the boundaries of the grid
 				for(int cChange = -1;cChange < 2;cChange++){
 					if(c + cChange >= 0 && r + cChange < myGrid.length){
-						if(myGrid[r+rChange][c+cChange].isDead()) numDeadCells++;
+						if(!myGrid[r+rChange][c+cChange].isDead()) numAliveCells++;
 					}
 				}
 			}	
 		}
-		return numDeadCells; 
+		return numAliveCells; 
 	}
 	
 
