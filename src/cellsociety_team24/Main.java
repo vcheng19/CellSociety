842053e8431a1b1376cab1e68f049a3c1e57671a
Carolyn Yao <cyao42@gmail.com>
2016-02-03 01:28:20 -0500

    line in main

--- src/cellsociety_team24/Main.java
+++ src/cellsociety_team24/Main.java
@@ -21,8 +21,8 @@ public class Main extends Application {
         FileReader reader = new FileReader(new File("xml_files/gol.xml")); 
         s.setTitle(reader.readProperty("title"));
         GridInitializer gi= new GridInitializer(root, reader);
-        //gi.test();
         gi.makeGrid();
+        //Simulator sim = new Simulator();
         s.setScene(scene);
         s.show();
     }
