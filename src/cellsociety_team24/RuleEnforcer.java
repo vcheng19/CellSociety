b639b13851b657376d21214a7594246e7d97db23
Cody <codyli332@gmail.com>
2016-02-03 10:39:16 -0500

    Wrote updateState method

--- src/cellsociety_team24/RuleEnforcer.java
+++ src/cellsociety_team24/RuleEnforcer.java
@@ -4,23 +4,35 @@ import java.util.*;
 
 
 public abstract class RuleEnforcer {
-	int gridsize; //FILLER
+	Cell[][] myGrid; 
 	public RuleEnforcer() {
 		// TODO Auto-generated constructor stub
 		
 	}
 	
+	public void iterateGrid(Cell[][] grid){
+		myGrid = grid; 
+		for(int x = 0; x < myGrid.length; x++){
+			for(int y = 0; y < myGrid.length; y++){
+				updateState(grid[x][y]); 
+			}
+		}
+		return; 
+	}
+	
 	public void updateState(Cell cell){
 		int row = cell.getX();
 		int col = cell.getY(); 
-		
-	
-			
+		checkNeighbors(row, col); 
+		return; 
 	}
 	
-	public void checkNeighbors(int xPos, int yPos){
-		
-	}
+//	public void checkNeighbors(int xPos, int yPos){
+//		int numDeadCells = 0; 
+//		if(xPos + 1 > myGrid.length){
+//			if(myGrid[xPos+1][yPos]) 
+//		}
+//	}
 	
 //	public boolean isCornerCell(int r, int c){
 //		if(r == 0 || r == gridsize - 1){
