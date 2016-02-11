554bbfd4428e4ca6e9b370e7221ce77c911f2a07
Virginia Cheng <vc54@duke.edu>
2016-02-11 17:07:16 -0500

    Update GridInitializer.java

--- src/gridinitializers/GridInitializer.java
+++ src/gridinitializers/GridInitializer.java
@@ -5,18 +5,30 @@ import javafx.scene.Group;
 
 public abstract class GridInitializer {
 
-	FileReader reader;
-	Group g; 
-	final int WORLD_SIZE = 600;
-	int DIMENSION; 
-	String CELL_TYPE;
-	public Cell[][] grid;
+	private FileReader reader;
+	private Group g; 
+	private final int WORLD_SIZE = 600;
+	private int DIMENSION; 
+	private String cellType;
+	private Cell[][] grid;
 	
 	GridInitializer (Group gr, FileReader fr) { 
 		g = gr; 
 		reader = fr;
 		DIMENSION = Integer.parseInt(reader.readProperty("dimension"));
 		makeGrid();
+	}
+	
+	public int getDimension(){
+		return DIMENSION;
+	}
+	
+	public String getCellType(){
+		return cellType;	
+	}
+	
+	public Group getGroup(){
+		return g;
 	}
 	
 	public Cell[][] getGrid(){
