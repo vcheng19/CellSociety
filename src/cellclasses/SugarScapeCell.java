5f40e57dc19610f9bfcbfa8ebc450f36b218e8e7
Cody <codyli332@gmail.com>
2016-02-11 21:59:29 -0500

    Created methods for SugarScapeCell class as well as created SugarScapeGridInitializer class

--- src/cellclasses/SugarScapeCell.java
+++ src/cellclasses/SugarScapeCell.java
@@ -3,6 +3,7 @@ package cellclasses;
 import javafx.scene.Group;
 
 public class SugarScapeCell extends Cell{
+	boolean agent;
 	//Agent attributes
 	boolean gender; //True if female, false if male 
 	int vision;
@@ -13,10 +14,62 @@ public class SugarScapeCell extends Cell{
 	int sugarAmount;
 	int sugarGrowBackRate; 
 	int sugarGrowBackInterval; 
-
+	
 	public SugarScapeCell(Group root, double size, int x, int y) {
 		super(root, size, x, y);
-		// TODO Auto-generated constructor stub
+		agent = false;
+		
 	}
 	
+	public boolean isAgent(){
+		return agent; 
+	}
+	
+	public boolean isFemale() {
+		return gender;
+	}
+
+	public int getVision() {
+		return vision;
+	}
+
+	public int getSugarMetabolism() {
+		return sugarMetabolism;
+	}
+
+	public int getSugar() {
+		return sugar;
+	}
+
+	public int getSugarAmount() {
+		return sugarAmount;
+	}
+
+	public int getSugarGrowBackRate() {
+		return sugarGrowBackRate;
+	}
+
+	public int getSugarGrowBackInterval() {
+		return sugarGrowBackInterval;
+	}
+	
+	public void eatSugar(){
+		sugar++;
+	}
+	
+	public void generateSugar(){
+		sugarAmount++; 
+	}
+	
+	public void makeAgent(){
+		agent = true; 
+		int pick = (int) Math.random() * 1;
+		if(pick == 0) gender = true;
+		else gender = false; 
+	}
+	
+	public void killAgent(){
+		agent = false; 
+	}
+
 }
---
+++ src/gridinitializers/SugarScapeGridInitializer.java
@@ -0,0 +1,10 @@
+package gridinitializers;
+
+public class SugarScapeGridInitializer {
+	private int vision;
+	private int sugarMetabolism;
+	private int sugar; 
+	private int sugarAmount;
+	private int sugarGrowBackRate;
+	private int sugarGrowBackInterval; 
+}
