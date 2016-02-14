bc28d04acec65d9e4077f8c4148d8e7b2bde106b
Cody <codyli332@gmail.com>
2016-02-14 03:02:53 -0500

    Refactoring with vc54

--- src/gridinitializers/ForagingAntGridInitializer.java
+++ src/gridinitializers/ForagingAntGridInitializer.java
@@ -1,5 +1,6 @@
 package gridinitializers;
 
+import cellclasses.Cell;
 import cellclasses.ForagingAntCell;
 import cellsociety_team24.Grid;
 import filereadcheck.FileReader;
@@ -10,17 +11,17 @@ public class ForagingAntGridInitializer extends GridInitializer {
 	private final String nestYTag = "nesty"; 
 	private final String foodXTag = "foodx"; 
 	private final String foodYTag = "foody"; 
-	private final int initAnts = Integer.parseInt(reader.readProperty("initantsnest"));
+	private final int initAnts = Integer.parseInt(getReader().readProperty("initantsnest"));
 	
 	public ForagingAntGridInitializer(Grid thisGrid, Group gr, FileReader fr, boolean w) {
 		super(thisGrid, gr, fr, w);
 	}
 	
 	public void makeGrid() {
-		grid = new ForagingAntCell[DIMENSION][DIMENSION];
+		Cell[][] grid = new ForagingAntCell[getDimension()][getDimension()];
 		for (int i=0;i<grid.length;i++) { 
 			for (int j=0;j<grid[0].length;j++) { 
-				ForagingAntCell cell = new ForagingAntCell(g, WORLD_SIZE/DIMENSION, i, j);
+				ForagingAntCell cell = new ForagingAntCell(i, j);
 				grid[i][j] = cell;
 				if (doConfigCell(nestXTag, nestYTag, i, j)) cell.makeNest(initAnts); 
 				if (doConfigCell(foodXTag, foodYTag, i, j)) cell.makeFood();
@@ -29,7 +30,7 @@ public class ForagingAntGridInitializer extends GridInitializer {
 	}
 
 	@Override
-	public void addAttributes() {
+	public void addAttributes(Cell[][] grid) {
 		// TODO Auto-generated method stub
 		
 	}
